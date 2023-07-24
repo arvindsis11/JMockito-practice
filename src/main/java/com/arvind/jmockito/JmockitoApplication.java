11ffaf9eb5d78af0dd496e4c47ac7302e2f0a414
arvind sisodia <57572062+arvindsis11@users.noreply.github.com>
2023-07-24 15:39:37 +0530

    initial commit test

---
+++ src/main/java/com/arvind/jmockito/JmockitoApplication.java
@@ -0,0 +1,13 @@
+package com.arvind.jmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arvind/jmockito/JmockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arvind.jmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
